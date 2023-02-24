f8311dc3bb4e647d6dfaf363160b916c12b27061
Angelinushka <116791003+Proby000@users.noreply.github.com>
2023-02-24 10:16:06 +0300

    Первый

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((elem, i) => elem === arr2[i]);
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(user, gender) {
+    let result = user.filter(elem => elem.gender.includes(gender)).map(elem => elem.age).reduce((acc, i, idx, arr) => {
+        acc += i;
+        if (idx === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0)
+
+    return user === 0 ? 0 : result;
 }
+
